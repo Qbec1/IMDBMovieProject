38ee12736d280367c4cfee8f844b8ff4a194bd0d
berkan aydoğan <aydogan-berkan@hotmail.com>
2025-07-18 10:56:49 +0300

    Business katmanına Generic Repository Pattern Kullanımı Eklendi

---
+++ IMDBMovieProject1/Abstract/IService.cs
@@ -0,0 +1,26 @@
+﻿using IMDBMovieProject.Entities.Entities;
+using System.Linq.Expressions;
+
+namespace IMDBMovieProject.Business.Abstract
+{
+    public interface IService<T> where T  : class, IEntity, new()
+    {
+        //Senkron metotlar
+        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T,bool>> expression);
+        IQueryable<T> GetQueryable();
+        T Get(Expression<Func<T, bool>> expression);
+        T Find(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        int SaveChanges();
+
+        //Asenkron metotlar
+        Task<T> FindAsync(int id);
+        Task<T> GetAsync(Expression<Func<T, bool>> expression);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task<int> SaveChangesAsync();
+    }
+}
